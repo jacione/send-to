e9478bf38d4ef3e9c5a412c931eca0e62bfd2686
Nick Porter <jacione@byu.edu>
2025-03-19 12:14:55 -0600

    Improve and simplify performance
    - Recompile all scripts if utils has been changed.
    - Skip creating shortcut if it already exists.
    - Update script records only AFTER recompiling
    - Play a beep at the end of the installation.

--- install.py
+++ install.py
@@ -1,8 +1,7 @@
-import os
 import subprocess as sub
 from pathlib import Path
 import importlib
-import argparse
+import winsound
 
 import win32com.client as wcom
 from win32com.shell import shellcon, shell as winshell
@@ -23,35 +22,44 @@ scripts = [
 def check_file_changed(name):
     script = Path(__file__).resolve().parent / f"src/{name}.py"
     record = Path(__file__).resolve().parent / f"data/{name}.txt"
-    if not record.exists():
-        record.write_text(script.read_text())
+    try:
+        if record.read_text() != script.read_text():
+            record.write_text(script.read_text())
+            return True
+        else:
+            return False
+    except FileNotFoundError:
         return True
-    elif record.read_text() != script.read_text():
-        record.write_text(script.read_text())
-        return True
-    else:
-        return False
 
 
-def main(recompile=False):
+def make_shortcut(title):
+    return Path(f"{send_to.as_posix()}/{title}.lnk").exists()
+
+
+def update_record(name):
+    script = Path(__file__).resolve().parent / f"src/{name}.py"
+    record = Path(__file__).resolve().parent / f"data/{name}.txt"
+    record.write_text(script.read_text())
+
+
+def main():
     shell = wcom.Dispatch("WScript.Shell")
+    recompile = check_file_changed("utils")
     for name in tqdm.tqdm(scripts):
         mod = importlib.import_module(f"src.{name}")
-        if recompile and check_file_changed(name):
-            sub.run(f"pyinstaller src/{mod.INFO['name']}.py --clean --onefile",
+        if recompile or check_file_changed(name):
+            sub.run(f"pyinstaller src/{name}.py --clean --onefile",
                     stdout=sub.DEVNULL, stderr=sub.DEVNULL
                     )
-        shortcut = shell.CreateShortCut(f"{send_to.as_posix()}/{mod.INFO['title']}.lnk")
-        shortcut.IconLocation = f"{dist.as_posix()}/{mod.INFO['name']}.exe"
-        shortcut.Targetpath = f"{dist.as_posix()}/{mod.INFO['name']}.exe"
-        shortcut.save()
+            update_record(name)
+        if make_shortcut(mod.INFO['title']):
+            shortcut = shell.CreateShortCut(f"{send_to.as_posix()}/{mod.INFO['title']}.lnk")
+            shortcut.IconLocation = f"{dist.as_posix()}/{mod.INFO['name']}.exe"
+            shortcut.Targetpath = f"{dist.as_posix()}/{mod.INFO['name']}.exe"
+            shortcut.save()
+    update_record("utils")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog='install.py',
-        description='Installs scripts so that they are accessible from the right-click "Send to... menu',
-    )
-    parser.add_argument("--recompile", action='store_true')
-    args = parser.parse_args()
-    main(args.recompile)
+    main()
+    winsound.MessageBeep()
